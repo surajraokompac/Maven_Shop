c271876ddec1166be4059ca583e508cc9ec0d8a3
AS00608741 <AS00608741@BSDPCS2964313.TechMahindra.com>
2019-05-20 15:53:09 +0530

    delete

--- Maven_Shop/src/main/java/com/dao/loginValidate.java
+++
@@ -1,7 +0,0 @@
-package com.dao;
-
-import com.domin.login;
-
-public interface loginValidate {
-public String loginValidate(login log);
-}
--- Maven_Shop/src/main/java/com/dao/registerDao.java
+++
@@ -1,7 +0,0 @@
-package com.dao;
-
-import com.domin.register;
-
-public interface registerDao {
-public String registerEmp(register regis );
-}
